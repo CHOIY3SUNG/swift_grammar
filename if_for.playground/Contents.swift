a9ee7e13d4f0ac7850ba17ef6134142f5f17eb4c
CHOIYESUNG <choiyesung2848@gmail.com>
2022-06-07 00:27:26 +0900

    first commit

---
+++ if_for.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var a : Bool = true;
+
+if a {
+    print(a)
+}
+if !a {
+    print(a)
+} else {
+    print(a)
+}
+
+let num = 10;
+var sumofnum : Int = 0;
+var listsum : [Int] = [];
+
+for i in 0..<num {
+    sumofnum += i;
+}
+print(sumofnum)
+
+for i in 0...num where i % 2 == 0 {
+    listsum.append(i)
+}
+print(listsum)
