b979cf7a44b5141118a9c69123a212ac4a2c92ab
CHOIY3SUNG <choiyesung2848@naver.com>
2022-06-09 19:59:01 +0900

    Class

--- class_.playground/Contents.swift
+++ class_.playground/Contents.swift
@@ -1,4 +1,21 @@
 import UIKit
 
+class Dog {
+    var name: String = ""
+    var age: Int = 0
+    
+    init(){
+        
+    }
+    
+    func introduce() {
+        print("my name is \(name), age is \(age)")
+    }
+}
 
-
+var dog = Dog()
+dog.name = "choco"
+dog.age = 2
+print(dog.name)
+print(dog.age)
+dog.introduce()
