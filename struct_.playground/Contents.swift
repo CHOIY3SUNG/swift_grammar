0dbb29bfa95d8686b1a129d941099780caae33f0
CHOIYESUNG <choiyesung2848@gmail.com>
2022-06-09 18:10:45 +0900

    struct

---
+++ struct_.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+struct User {
+    var nickname: String
+    var age: Int
+    
+    func saymyage() {
+        print(age)
+    }
+}
+
+var user = User(nickname: "Y3SUNG", age: 18)
+
+print(user.nickname)
+print(user.age)
+user.nickname = "yesung"
+user.age += 1
+print(user.nickname)
+user.saymyage()
