e014cfd0f01270d8cbb2549f5322f62bf730b605
CHOIY3SUNG <choiyesung2848@naver.com>
2022-06-11 11:11:41 +0900

    Protocol

--- Protocol.playground/Contents.swift
+++ Protocol.playground/Contents.swift
@@ -1,3 +1,53 @@
 import UIKit
 
-var greeting = "Hello, playground"
+protocol SomeProtocol {
+    
+}
+protocol SomeProtocol2 {
+    
+}
+
+struct SomeStructrue: SomeProtocol, SomeProtocol2 {
+    
+}
+
+protocol FirstProtocol {
+    var name: Int { get set }
+    var age: Int { get }
+}
+
+protocol AnotherProtocol {
+    static var someTypeProperty: Int { get set }
+}
+
+protocol FullNames {
+    var fullname: String { get set }
+    func printFullname()
+}
+
+struct Person : FullNames {
+    var fullname: String
+    
+    func printFullname() {
+        print(fullname)
+    }
+}
+
+protocol SomeProtocol3 {
+    func someTypeMethod()
+}
+
+protocol SomeProtocol4 {
+    init(someParameter: Int)
+}
+
+protocol SomeProtocol5 {
+    init()
+}
+
+class SomeClass: SomeProtocol5 {
+    required init() {
+        
+    }
+    
+}
