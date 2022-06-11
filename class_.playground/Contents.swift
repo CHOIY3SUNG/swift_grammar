39fcb10c6c987da33e928afaee181529f89cdb36
CHOIY3SUNG <choiyesung2848@naver.com>
2022-06-11 10:24:00 +0900

    상속추가

--- class_.playground/Contents.swift
+++ class_.playground/Contents.swift
@@ -19,3 +19,57 @@ dog.age = 2
 print(dog.name)
 print(dog.age)
 dog.introduce()
+
+
+class Vehicle {
+    var curentSpeed = 0.0
+    var description: String {
+        return "traceling at \(curentSpeed) mailes per hour"
+    }
+    func makeNoise() {
+        print("speaker on")
+    }
+}
+
+class Bicycle: Vehicle {
+    var hasBasketm = false
+    
+}
+
+var bicycle = Bicycle()
+bicycle.curentSpeed = 15.0
+print(bicycle.curentSpeed)
+
+class Train: Vehicle {
+    override func makeNoise() {
+        super.makeNoise()
+        print("choo choo")
+    }
+}
+
+let train = Train()
+train.makeNoise()
+
+class Car: Vehicle {
+    var gear = 1
+    override var description: String {
+        return super.description + "in  gear \(gear)"
+    }
+}
+
+let car = Car()
+car.curentSpeed = 30.0
+car.gear = 2
+print(car.description)
+
+class AutomaticCat: Car {
+    override var curentSpeed: Double {
+        didSet {
+            gear = Int(curentSpeed / 10) + 1
+        }
+    }
+}
+
+let automatic = AutomaticCat()
+automatic.curentSpeed = 35.0
+print("AutomaticCar : \(automatic.description)")
