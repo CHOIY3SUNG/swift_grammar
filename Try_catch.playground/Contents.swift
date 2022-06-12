d8b592044080304e465ba705500e9312b5e2684d
CHOIY3SUNG <choiyesung2848@naver.com>
2022-06-12 11:15:50 +0900

    예외처리

--- Try_catch.playground/Contents.swift
+++ Try_catch.playground/Contents.swift
@@ -1,3 +1,31 @@
 import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+enum PhoneError: Error {
+    case unknown
+    case batterryLow(batteryLevel: Int)
+}
+
+//throw PhoneError.batterryLow(batteryLevel: 20)
+
+func checkPhoneBatteryStatus(batteryLevel: Int) throws -> String {
+    guard batteryLevel != -1 else { throw PhoneError.unknown }
+    guard batteryLevel > 20 else { throw PhoneError.batterryLow(batteryLevel: 20)}
+    return "배터리 상태가 정상입니다"
+}
+
+do {
+    try checkPhoneBatteryStatus(batteryLevel: 20)
+} catch PhoneError.unknown {
+    print("알 수 없는 에러입니다.")
+} catch PhoneError.batterryLow(let baterryLebel) {
+    print("배터리 전원 부족 남은 배터리 : \(baterryLebel)%")
+} catch {
+    print("그 외 오류 발생 : \(error)")
+}
+
+let status = try? checkPhoneBatteryStatus(batteryLevel: 30)
+print(status)
+
+let status2 = try! checkPhoneBatteryStatus(batteryLevel: 30)
+print(status2)
